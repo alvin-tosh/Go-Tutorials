1b24a6998c031b810c6f21f2649cafa1528bce37
Palpatine <alvingitonga14@gmail.com>
2022-03-29 09:57:31 +0300

    Update Variables.go

--- Variables.go
+++ Variables.go
@@ -18,3 +18,14 @@ func main() {
   p = "Follow me on Twitter @_Lord_Heathen"             // Strings must contain double quotes
 	fmt.Println(x, y, z, p)                               // 20 true 75 Follow me on Twitter @_Lord_Heathen
 }
+
+//Short variable declaration: Another way is to use the := operator to initialize and assign to variables in one go. 
+
+
+/*func main() {
+	x := 20                                       // Initialize and assign to a single variable
+	y, z := true, 75.0 			      // Initialize and assign to multiple variables
+	p := "string"
+	fmt.Println(a, b, c, d) // 20 true 75 Follow me on Twitter @_Lord_Heathen
+}
+*/
