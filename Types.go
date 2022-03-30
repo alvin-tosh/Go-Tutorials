4383263b1d3f284023be67d409d9ba8b5562a4af
Palpatine <alvingitonga14@gmail.com>
2022-03-30 09:11:25 +0300

    Update Types.go
    # output
    user-specified types:
     int32 float32 complex128 uint16
    
    default types:
     int float64 bool bool string

--- Types.go
+++ Types.go
@@ -1,1 +1,21 @@
-<>
+package main
+
+import "fmt"
+
+func main() {
+  
+        /* User specified types */
+        const w int32 = 150          // 32-bit integer
+        const x float32 = 50.5       // 32-bit float
+        var y complex128 = 1 + 4i    // 128-bit complex number
+        var z uint16 = 18            // 16-bit unsigned integer
+
+        /* Default types */
+        n := 35                                  // int
+        pi := 3.14                               // float64
+        p, r := true, false                      // bool
+        h := "Learning types in Go is AWESOME!"  // string
+
+        fmt.Printf("user-specified types:\n %T %T %T %T\n", w, x, y, z)
+        fmt.Printf("default types:\n %T %T %T %T %T\n", n, pi, p, r, h)
+}
