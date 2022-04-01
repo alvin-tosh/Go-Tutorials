4960c4ca07e07ae1be898ef978168cd275b0b55f
Palpatine <alvingitonga14@gmail.com>
2022-04-01 08:53:11 +0300

    Update Concurrency.go

--- Concurrency.go
+++ Concurrency.go
@@ -13,6 +13,11 @@ import (
 	"fmt"
 )
 
+/*
+First, we create a channel that will be common to all goroutines. Then we start 5 goroutines and pass the channel as an argument. 
+Inside each goroutine, we write the gopher id to standard output as soon the gopher starts cooking the dish. We then send the gopher id from the goroutine back to the caller. 
+From there, we’re back to the body of the main function where we receive the gopher id and record their finishing time.
+*/
 func main() {
 	c := make(chan int) // Create a channel to pass ints
 	for i := 0; i < 5; i++ {
@@ -30,3 +35,9 @@ func cookingGopher(id int, c chan int) {
 	fmt.Println("gopher", id, "started cooking")
 	c <- id // Send a value back to main
 }
+
+/*
+Since we’re dealing with concurrent code, we lose the ability to predict the order of the output, however, we can observe how the channel blocks the execution, as a goroutine has to wait until the channel is available before it can send an id. 
+One POSIBLE output is included below.
+Keep in mind that we’re probably using more goroutines than the number of cores on our machine, hence it’s likely that a single core is time-multiplexed to simulate the concurrency.
+*/
