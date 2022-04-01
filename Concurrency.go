d95964e2fa8740a22c64e605b3bbfe91820368ba
Palpatine <alvingitonga14@gmail.com>
2022-04-01 08:43:50 +0300

    Update Concurrency.go

--- Concurrency.go
+++ Concurrency.go
@@ -7,3 +7,26 @@ In the example below, we’ll consider 5 goroutines that run in parallel. Let’
 This is a timed contest and whoever finishes their dish first wins. Let’s see how we can simulate this contest using Go’s concurrency features.
 
 */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int) // Create a channel to pass ints
+	for i := 0; i < 5; i++ {
+		go cookingGopher(i, c) // Start a goroutine
+	}
+
+	for i := 0; i < 5; i++ {
+		gopherID := <-c // Receive a value from a channel
+		fmt.Println("gopher", gopherID, "finished the dish")
+	} // All goroutines are finished at this point
+}
+
+/* Notice the channel as an argument */
+func cookingGopher(id int, c chan int) {
+	fmt.Println("gopher", id, "started cooking")
+	c <- id // Send a value back to main
+}
